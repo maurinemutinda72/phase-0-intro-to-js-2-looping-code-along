5ab6e3c585a0072f1ec9b6f0414b64749341b4dd
moureen mutinda <maurinemutinda72@gmail.com>
2024-12-19 11:57:47 +0300

    completed task

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = []; // Create an empty array to hold the messages
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message); // Add the message to the array
+    }
+  
+    return messages; // Return the array of messages
+  }
+  
